9b1bae286fd101e8000e52b37ac8af5ce0ded367
danpgomez <dapgo3@gmail.com>
2022-04-01 19:24:54 -0400

    Complete fragment code implementation

--- app/src/main/java/com/example/guessinggame/GameFragment.kt
+++ app/src/main/java/com/example/guessinggame/GameFragment.kt
@@ -1,59 +1,101 @@
 package com.example.guessinggame
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.guessinggame.databinding.FragmentGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
+
+    private val words = listOf("Android", "Activity", "Fragment")
+    private val secretWord = words.random().uppercase()
+
+    // String to display in the secret word field
+    private var secretWordDisplay = ""
+
+    private var correctGuesses = "" // Perhaps consider changing this to correctlyGuessedLetters
+    private var incorrectGuesses = ""
+    private var livesLeft = 8
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        secretWordDisplay = deriveSecretWordDisplay()
+        updateScreen()
+
+        binding.guessButton.setOnClickListener {
+            val playerGuess = binding.guess.text.toString().uppercase()
+            makeGuess(playerGuess)
+            binding.guess.text = null
+            updateScreen()
+
+            if (isWon() || isLost()) {
+                val action =
+                    GameFragmentDirections.actionGameFragmentToResultFragment(wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun updateScreen() {
+        binding.word.text = secretWordDisplay
+        binding.lives.text = "You have $livesLeft lives left."
+        binding.incorrectGuesses.text = "Incorrect guesses: $incorrectGuesses"
+    }
+
+    private fun deriveSecretWordDisplay(): String {
+        var display = ""
+        secretWord.forEach { letter ->
+            display += checkLetter(letter.toString())
+        }
+
+        return display
+    }
+
+    private fun checkLetter(str: String) = when (correctGuesses.contains(str)) {
+        true -> str
+        false -> "_"
+    }
+
+    private fun makeGuess(guess: String) {
+        if (guess.length == 1) { // Why do we need this check?
+            if (secretWord.contains(guess)) {
+                correctGuesses += guess
+                secretWordDisplay = deriveSecretWordDisplay()
+            } else {
+                incorrectGuesses += "$guess"
+                livesLeft--
             }
+        }
+    }
+
+    private fun isWon() = secretWord.equals(secretWordDisplay, true)
+    private fun isLost() = livesLeft <= 0
+
+    private fun wonLostMessage(): String {
+        var message = ""
+        if (isWon()) message = "You WON!"
+        else if (isLost()) message = "You lost."
+        message += " The correct word was $secretWord."
+        return message
     }
 }
